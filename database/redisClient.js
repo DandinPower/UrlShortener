94c0285088cdbecdd52b171b1d4d2b5ef7511cad
DandinPower <t108590045@ntut.org.tw>
2022-02-19 05:32:09 +0800

    update

---
+++ database/redisClient.js
@@ -0,0 +1,36 @@
+const redis = require('redis')
+const redisClient = redis.createClient({ legacyMode: true })
+redisClient.connect()
+
+//透過key取得json
+async function getHash(key) {
+    return new Promise((resolve, reject) => {
+        redisClient.hGetAll(key, (err, res) => {
+            if (err) {
+                reject(err)
+            } else {
+                if (res.length != 0) {
+                    let value = {
+                        "originalUrl": res[1],
+                        "expireAt": new Date(res[3])
+                    }
+                    resolve(value)
+                }
+                else {
+                    resolve(null)
+                }
+            }
+        })
+    })
+}
+
+//設置hash,value為json
+async function setHash(key, value) {
+    await redisClient.hSet(key, 'originalUrl', value.originalUrl)
+    await redisClient.hSet(key, 'expireAt', value.expireAt.toISOString())
+}
+
+module.exports = {
+    getHash,
+    setHash
+}
---
+++ test/redisTest.js
@@ -0,0 +1,37 @@
+let { assert, should } = require('chai')
+const chaiHttp = require('chai-http')
+const chai = require('chai')
+const Url = require('../models/Url')
+const Redis = require('../database/redisClient')
+describe('RedisTest', async () => {
+
+    //初始化測試
+    before(async () => {
+        chai.use(chaiHttp)
+    })
+
+    //初始化每個測試
+    beforeEach(async () => {
+
+    });
+
+    //關閉每個測試
+    afterEach(async () => {
+
+    })
+
+    it("測試setHash", async () => {
+        let key = 'testSetHash'
+        let value = {
+            "originalUrl": "http://test.com",
+            "expireAt": new Date("2021-02-17T10:55:00.000Z")
+        }
+        await Redis.setHash(key, value)
+    })
+
+    it("測試getHash", async () => {
+        let key = 'testSetHash'
+        const result = await Redis.getHash(key)
+        console.log(result)
+    })
+})
