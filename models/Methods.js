7f75d64b316c6dee89856beb5ebab7bd518d5726
DandinPower <t108590045@ntut.org.tw>
2022-02-20 01:13:46 +0800

    確認時間

--- models/Methods.js
+++ models/Methods.js
@@ -1,6 +1,8 @@
 //回傳傳入的時間是否大於現在時間
 function compareTime(time) {
     var nowTime = new Date(Date.now())
+    console.log(`給定時間為 ${time.toISOString()}`)
+    console.log(`現在時間為 ${nowTime.toISOString()}`)
     return nowTime <= time
 }
 
